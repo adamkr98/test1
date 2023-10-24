53a30c7e86eae3b74e8dd21fc11f00a3438846b2
adamkr98 <adamkrynski2@gmail.com>
2023-10-24 19:40:55 +0200

    removing old firebaseConfig and replacing with a new one

--- src/fbconfig.js
+++ src/fbconfig.js
@@ -4,14 +4,15 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD3MS3Hk6dS5V0CngT1UyRcljOMA-l72ZU",
-  authDomain: "signuploginfirebase-17ddc.firebaseapp.com",
-  databaseURL: "https://signuploginfirebase-17ddc-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "signuploginfirebase-17ddc",
-  storageBucket: "signuploginfirebase-17ddc.appspot.com",
-  messagingSenderId: "1012482307289",
-  appId: "1:1012482307289:web:6ca11695bd79aa4b59bac8"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
@@ -21,11 +22,3 @@ const database = getDatabase(app);
 export { auth, database };
 
 
-// export default {
-//   apiKey: "AIzaSyD3MS3Hk6dS5V0CngT1UyRcljOMA-l72ZU",
-//   authDomain: "signuploginfirebase-17ddc.firebaseapp.com",
-//   projectId: "signuploginfirebase-17ddc",
-//   storageBucket: "signuploginfirebase-17ddc.appspot.com",
-//   messagingSenderId: "1012482307289",
-//   appId: "1:1012482307289:web:6ca11695bd79aa4b59bac8",
-// };
